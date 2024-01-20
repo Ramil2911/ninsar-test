ab6e290aa74292795446cf8f268cda3d87e19aac
Ramil Gabdrakhmanov <ramil2911912@gmail.com>
2024-01-20 23:40:54 +0300

    add files

---
+++ Assets/Scripts/BlocksController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class BlocksController : MonoBehaviour
+{
+    public List<Material> materials;
+    public GameObject basicPrefab;
+
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+
+    private readonly FileAccessor _accessor = new FileAccessor();
+
+    private Renderer[,] _blocks;
+    private string _path = "";
+    private bool _updateFlag;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _blocks = new Renderer[3, 3];
+        for (var i = 0; i < 3; i++)
+        for (var j = 0; j < 3; j++)
+        {
+            var go = Instantiate(basicPrefab, new Vector3((-1 + i) * -1.5f, 0, (-1 + j) * 1.5f), Quaternion.identity,
+                transform);
+            _blocks[j, i] = go.GetComponent<Renderer>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var (rows, cols) = _accessor.Size;
+
+        //Inputs section (i dont like it but importing new input system is too much)
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            x = Mathf.Clamp(x - 1, 0, rows - 1);
+            _updateFlag = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            x = Mathf.Clamp(x + 1, 0, rows - 1);
+            _updateFlag = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            y = Mathf.Clamp(y - 1, 0, cols - 1);
+            _updateFlag = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            y = Mathf.Clamp(y + 1, 0, cols - 1);
+            _updateFlag = true;
+        }
+
+        // Blocks update section
+
+        if (!_updateFlag) return;
+        _updateFlag = false;
+
+        var matrix = _accessor[x, y];
+        for (var i = 0; i < 3; i++)
+        for (var j = 0; j < 3; j++)
+            if (matrix[i, j] != -1)
+            {
+                _blocks[i, j].enabled = true;
+                _blocks[i, j].material = materials[matrix[i, j] - 1];
+            }
+            else
+            {
+                _blocks[i, j].enabled = false;
+            }
+    }
+
+    public void UpdatePath(string path)
+    {
+        _path = path;
+    }
+
+    public void Read()
+    {
+        _accessor.Open(_path).ContinueWith(task =>
+            {
+                _updateFlag = task.Result;
+
+                if (!task.Result) return; //the following code is executed only when numbers are successfully loaded
+
+                var (rows, cols) = _accessor.Size;
+                var rnd = new Random();
+                x = rnd.Next(0, rows);
+                y = rnd.Next(0, cols);
+            }
+        );
+    }
+}
---
+++ Assets/Scripts/FileAccessor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class FileAccessor
+{
+    private const int Padding = 1;
+    private int _cols;
+    private bool _isInitialized;
+    private int[,] _numbers;
+
+    private int _rows;
+
+    /// <summary>
+    ///     Creates a 3x3 array which is a slice of array with (x,y) element in the middle
+    /// </summary>
+    /// <param name="x">X coordinate</param>
+    /// <param name="y">Y coordinate</param>
+    public int[,] this[int x, int y]
+    {
+        get
+        {
+            if (!_isInitialized) return new[,] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
+
+            //idk i dont want to write a size check
+
+            var window = new int[3, 3];
+
+            for (var i = -1; i < 2; i++)
+            for (var j = -1; j < 2; j++)
+                window[i + 1, j + 1] = _numbers[Padding + x + i, Padding + y + j];
+
+            return window;
+        }
+    }
+
+    public (int, int) Size => (_rows, _cols);
+
+
+    /// <summary>
+    ///     Starts a task which reads all values from the file, saves the file in class, does not check the validity
+    /// </summary>
+    /// <param name="path">Path to a .txt file</param>
+    /// <returns> Returns a Task with bool value where the result indicates if file was read successfully or not </returns>
+    public Task<bool> Open(string path)
+    {
+        if (!File.Exists(path)) return Task.FromResult(false);
+        return Task.Run(() =>
+        {
+            try
+            {
+                // Read numbers from file
+                var lines = File.ReadAllLines(path);
+                var rows = lines.Length;
+                var cols = lines[0].Length;
+
+                var ints = new int[rows, cols];
+
+                for (var i = 0; i < rows; i++)
+                for (var j = 0; j < cols; j++)
+                    ints[i, j] = int.Parse(lines[i][j].ToString());
+
+                // Create a new padded 2D array with increased dimensions
+                var numbers = new int[rows + 2 * Padding, cols + 2 * Padding];
+
+                // Fill the padded array with -1
+                for (var i = 0; i < rows + 2 * Padding; i++)
+                for (var j = 0; j < cols + 2 * Padding; j++)
+                    numbers[i, j] = -1;
+
+                // Copy the original array into the padded array
+                for (var i = 0; i < rows; i++)
+                for (var j = 0; j < cols; j++)
+                    numbers[i + Padding, j + Padding] = ints[i, j];
+
+                _numbers = numbers;
+                _cols = cols;
+                _rows = rows;
+
+                _isInitialized = true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"Reading text file failed with the following exception\n{e.Message}");
+                return false;
+            }
+
+            return true;
+        });
+    }
+}
